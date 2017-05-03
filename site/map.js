70a419e87620c65c131ae513688593b6924dbee2
John Chapin <john.chapin@gmail.com>
2017-05-03 19:16:38 -0400

    Add local visualisation website that pulls from API GW

---
+++ site/map.js
@@ -0,0 +1,43 @@
+// Center on United States
+var mymap = L.map('map').setView([39.833333, -98.583333], 5);
+
+// Use free CartoDB tileset (served from a CDN and no API key required, yay!)
+L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png', {
+maxZoom: 18, attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy; <a href="https://carto.com/attribution">CARTO</a>'
+}).addTo(mymap);
+
+// Call out to API Gateway, ask for current locations
+var xhr = new XMLHttpRequest();
+// Replace this URL
+xhr.open('GET', 'https://o30vnzv5ci.execute-api.us-west-2.amazonaws.com/prod/locations');
+xhr.send(null);
+
+// Add locations to map
+xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4) { // readyState 4 means the request is finished
+        if (xhr.status === 200) {
+            var featureCollection = JSON.parse(xhr.responseText);
+            L.geoJSON(featureCollection, {
+                pointToLayer: function (feature, latlng) {
+                        var t = feature.properties.temperature;
+                        var c = "#ffffff";
+                        if (t > 90) {
+                            c = "#ff0000";
+                        } else if (t > 65) {
+                            c = "#00ff00";
+                        } else {
+                            c = "#0000ff";
+                        }
+                        var options = {color: c};
+                        return L.circleMarker(latlng, options);
+                    },
+                onEachFeature: function (feature, layer) {
+                        if (feature.properties && feature.properties.temperature) {
+                                layer.bindPopup(feature.properties.temperature.toString());
+                        }
+                }}).addTo(mymap);
+        } else {
+            console.log('Error: ' + xhr.status);
+        }
+    }
+};
